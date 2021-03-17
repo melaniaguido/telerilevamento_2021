05d031c1f0c9014b40e93468b1115f78b8199501
melaniaguido <80513332+melaniaguido@users.noreply.github.com>
2021-03-17 12:51:12 +0100

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -15,3 +15,16 @@ p224r63_2011 #rasterbrick cioè una serie di bande in formato raster
 
 plot(p224r63_2011) #Plot è una funzione generica per la stampa di oggetti R 
 
+#colorRampPalette comando con cui siamo noi a stabilire la variazione dei colori 
+#per spiegare a R che sono tutti elementi della stessa cosa, vanno racchiusi nello  stesso vettore/array
+#inseriamo quindi una c davanti ai colori ed una parentesi che racchiude tutto 
+
+cl <- colorRampPalette (c("black","grey","light grey"))  (100) #100 il numero di livelli che assegno io 
+
+plot(p224r63_2011, col=cl) #primo argomento immagine che vogliamo plottare, secondo argomento il colore che vogliamo dare 
+
+#colour change -> new, impostiamo dei colori a piacere 
+
+cl <- colorRampPalette (c("blue","pink","red","yellow","green")) (100)
+plot(p224r63_2011, col=cl)
+
