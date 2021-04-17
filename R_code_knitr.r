6a8622c5aeec59a8b7af4eb59c2cd605ffa0f9a3
melaniaguido <80513332+melaniaguido@users.noreply.github.com>
2021-04-17 11:26:21 +0200

    Update R_code_knitr.r

--- R_code_knitr.r
+++ R_code_knitr.r
@@ -1,7 +1,12 @@
 #R_code_knitr.r
 
-#setwd("C:/lab/") 
+setwd("C:/lab/") 
 
 #knitr all'interno di R genera un report
 library(knitr) #richiamo il pacchetto knitr 
+
+#Utilizziamo la funzione stitch, come prima cosa dobbiamo inserire come si chiama il nostro codice 
+#gli altri argomenti sono il template che utilizziamo, misc e il file del template
+#inseriamo poi il nome del pacchetto da utilizzare 
+stitch("R_code_Greenland.r.txt", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
 
