c35d038b7f77583a75d23fb67d3a54d87f26fde0
melaniaguido <80513332+melaniaguido@users.noreply.github.com>
2021-04-23 12:56:01 +0200

    Update R_code_classification.r

--- R_code_classification.r
+++ R_code_classification.r
@@ -30,5 +30,38 @@ set.seed(42)
 socl <- unsuperClass(so, nClasses=20)
 plot(socl$map)
 
+#Day 2 
+
+#Grand Canyon 
+#https://landsat.visibleearth.nasa.gov/view.php?id=80948
+# When John Wesley Powell led an expedition down the Colorado River and through the Grand Canyon in 1869, he was confronted with a daunting landscape. At its highest point, the serpentine gorge plunged 1,829 meters (6,000 feet) from rim to river bottom, making it one of the deepest canyons in the United States. In just 6 million years, water had carved through rock layers that collectively represented more than 2 billion years of geological history, nearly half of the time Earth has existed.
+
+#Carichiamo le librerie 
+library(raster)
+library(RStoolbox)
+#settiamo la wd 
+setwd("C:/lab/")
+
+#L'immagine che utilizziamo è una RGB, è formata quindi da tre livelli, per caricare questo tipo di immagine utilizziamo la funzione brick 
+gc <- brick("dolansprings_oli_2013088_canyon_lrg.jpg")
+gc
+
+#Visualizziamo la nostra immagine tramite la funzione plotRGB
+#Abbiamo  un oggetto raster con più strati e attraverso questa funzione andiamo a plottarli 
+plotRGB(gc, 1,2,3, stretch="lin")
+#Cambiamo la visualizzazione tramite lo stretching da lineare a histogram dove vengono utitlizzate tutte le bande possibili per visualizzare l'immagine 
+plotRGB(gc, 1,2,3, stretch="hist")
+
+#Utilizziamo la funzione unsuperClass
+gcc2 <- unsuperClass(gc, nClasses=2)
+gcc2 #Visualizzo i dati 
+plot(gcc2$map)
+
+#Aumentiamo in numero di classi 
+gcc4 <- unsuperClass(gc, nClasses=4)
+gcc4 #Visualizzo i dati 
+plot(gcc4$map)
+
+
 
 
